c006ebcb89442fb169387c12ba45aa1e7abfb1c3
Connor Williams <createdbyconnor@connors-MBP.lan>
2015-02-18 22:21:46 +0000

    grunt configured and ready to go - working on css next

--- gruntfile.js
+++ gruntfile.js
@@ -1,1 +1,30 @@
-gruntfile.js
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+	  sass: {                              // Task 
+	    dist: {                            // Target 
+	      options: {                       // Target options 
+	        style: 'expanded'
+	      },
+	      files: {                         // Dictionary of files 
+	        'css/core.css': 'dev/scss/build.scss',       // 'destination': 'source' 
+	      }
+	    }
+	  },
+	  watch: {
+		  scripts: {
+		    files: ['dev/**/*.scss'],
+		    tasks: ['sass'],
+		    options: {
+		      spawn: false,
+		    },
+		  },
+		}
+	});
+	 
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['sass']);
+	grunt.registerTask('monitor', ['watch']);
+};
